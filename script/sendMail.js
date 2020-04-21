31cd8e8c0079ce3a7a930e1ad5554346a9e380b0
Fabio Cortes <fcor14@hotmail.com>
2020-04-21 18:17:42 -0500

    Added super basic validation

--- script/sendMail.js
+++ script/sendMail.js
@@ -1,8 +1,16 @@
+/* global MicroModal*/
+
 const email = document.getElementById("email");
 const msg = document.getElementById("msg");
 const sendButton = document.getElementById("send");
 
 const handleSendEmail = () => {
+    if (email.validity.valid || msg.value.length === 0) {
+      // formulario no válido!
+      console.log("Not valid");
+      return;
+    }
+    sendButton.disabled = true;
     const data = {
                     email: email.value, 
                     text: msg.value,
@@ -19,10 +27,15 @@ const handleSendEmail = () => {
         return response.json();
       })
       .then(function(myJson) {
+        sendButton.disabled = false;
         console.log(myJson);
         // Acá debe ir la acción de envío correcto
+        MicroModal.close('modal-1');
+        email.value = "";
+        msg.value = "";
       })
       .catch(function(error) {
+        sendButton.disabled = false;
         console.log("Something went wrong");
         // Acá debe ir la acción de envío fallido
       });
